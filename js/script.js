6d28539e85a90ddb8f0cadab2e376c45df2d7071
kongphui <kongphui@2nd-Approach-Methodology.local>
2017-10-02 22:07:45 +0800

    fixed navber ontop
    somehow nav bar is successfully fixed on top!

--- js/script.js
+++ js/script.js
@@ -36,3 +36,79 @@ $(document).ready(function () {
         $('.navbar-collapse.in').collapse('hide');
     });
 });
+
+function makeThankText(){
+    var newFriendName="";
+    var newFriendEmail="";
+    var acknowText="";
+
+    newFriendName = document.getElementById("name-id").value;
+    newFriendEmail = document.getElementById("email-id").value;
+
+    document.getElementById("ackText").innerHTML = newFriendName + ", nice to have you as a new friend!"+"<br>"+"I shall contact you via email "+newFriendEmail ;
+
+    // acknowText = newFriendName + ", nice to have you as a new friend!"+"\n"+"I shall contact you via email "+newFriendEmail ;
+    // alert(acknowText);
+}
+
+$(function () {
+    
+        // on submitting the form
+        $('form').submit(function (event) {
+            // prevent the default action of reloading the page
+            event.preventDefault();
+    
+            var sendData = {};
+            $(event.target.nodeName + ' :input').each(function () {
+                sendData[this.name] = $(this).val();
+            });
+            console.log(sendData);
+    
+            var posting = $.ajax({
+                type: 'POST',
+                url: $(event.target.nodeName).prop('action'),
+                data: sendData
+            });
+    
+            posting.done(function (response) {
+                console.log(response);
+                $('#alert-id').prop('hidden', false);
+                $('form :input').each(function () {
+                    $(this).val('');
+                })
+            });
+            posting.fail(function (response) {
+                console.log(response);
+            });
+        });
+    
+        // RESPONSE ALERT WINDOW-------------------------------------------------------------------------------
+        /* include the following HTML to use:
+        <div class="form-group">
+            <button type="submit" class="btn btn-default my-btn form-control" id="submit-id">submit</button>                   
+            <div class="alert alert-danger alert-dismissible fade in" hidden id="alert-id">
+                <button type="button" class="close" id="close-id"><span>&times;</span></button>
+                Thank you! We'll get in touch.
+            </div>
+        </div>
+        */
+    
+        // on clicking the X button
+        $('#close-id').click(function () {
+            // hide the alert panel by adding the hidden property
+            $('#alert-id').prop('hidden', true);
+        });
+    
+    });
+    
+    // When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
